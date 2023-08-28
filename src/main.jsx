b46c0f998d04df9438951e4b244014876674c4b6
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-08-28 14:34:23 +0600

    data load

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Main from './components/Layout/Main.jsx';
+import Orders from './components/Orders/Orders.jsx';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader:() => fetch('tShirts.json'),
+      },
+      {
+        path:'order',
+        element:<Orders></Orders>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
